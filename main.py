4c4084903919be7a5c3576e51821211592fa9122
Rarelo <github@rarelo.net>
2021-12-24 17:59:30 -0800

    finishes hexagon render engine and added terminal to create polygons

--- main.py
+++ main.py
@@ -1,52 +1,37 @@
 import pygame
 from sys import exit
 import os
+import random
 
 #program code import
 import main_classes
 
 ## TODO:
-#add multiple polygons (placing them)
+#fix the broken rendering for the added terminal polygons
+#split the classes structure into mutliple files
+#raise the window resolution, higher resolutions? Fullscreen?
 #add movement functionality to polygons
 #add engine functionality nit features (MVP)
-#raise pixel sise for better animations
 
 
 #global vars
 WIDTH = 360 #make rendering system based on multiples?
-HEIGHT = 202
+HEIGHT = 202 #848, 480, also remove scaled from mode b/c it breaks vsync
+GAMESTATE = True
 
 #display settings
 pygame.display.set_caption('pixel war game')
-SCREEN = pygame.display.set_mode((WIDTH,HEIGHT),pygame.SCALED, pygame.OPENGL, vsync=1) #360 202?
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT), pygame.SCALED, vsync=1) #360 202?
 
 #create objects
-hexagon_group = pygame.sprite.LayeredUpdates()
-RenderEngine1 = main_classes.RenderEngine() #creates an instance of the render engine class
 
-hexagon1 = main_classes.Hexagon(100,100)
-RenderEngine1.create_polygons(hexagon1,None,None,hexagon_group)
+main_classes.RenderEngine1.create_polygons(None,None) #need this polygon so hexagon_list exists
+TextConsole1 = main_classes.TextConsole() #init console
 
-hexagon2 = main_classes.Hexagon(200,200)
-RenderEngine1.create_polygons(hexagon2,hexagon1,6,hexagon_group)
+#for i in range(5):
+#    RenderEngine1.create_polygons(main_classes.hexagon_list[i],random.randint(1,6))
 
-hexagon3 = main_classes.Hexagon(200,200)
-RenderEngine1.create_polygons(hexagon3,hexagon1,4,hexagon_group)
-
-hexagon4 = main_classes.Hexagon(200,200)
-RenderEngine1.create_polygons(hexagon4,hexagon2,4,hexagon_group)
-
-hexagon4 = main_classes.Hexagon(200,200)
-RenderEngine1.create_polygons(hexagon4,hexagon1,1,hexagon_group)
-
-hexagon5 = main_classes.Hexagon(200,200)
-RenderEngine1.create_polygons(hexagon5,hexagon1,2,hexagon_group)
-
-hexagon6 = main_classes.Hexagon(200,200)
-RenderEngine1.create_polygons(hexagon6,hexagon1,3,hexagon_group)
-
-
-hexagon_group = RenderEngine1.sort_hexagons(main_classes.hexagon_list, hexagon_group)
+main_classes.hexagon_group = main_classes.RenderEngine1.sort_polygons(main_classes.hexagon_list)
 
 #game loop
 while True:
@@ -65,6 +50,8 @@ while True:
                 main_classes.camera.w_pressed = True
             if event.key == pygame.K_s: #need to make hold down button work
                 main_classes.camera.s_pressed = True
+            if event.key == pygame.K_BACKQUOTE: #need to make hold down button work
+                TextConsole1.user_command_input()
         if event.type == pygame.KEYUP: #for buttons that need to be held down
             if event.key == pygame.K_a:
                 main_classes.camera.a_pressed = False
@@ -79,7 +66,8 @@ while True:
     pygame.display.update()
     main_classes.clock.tick(60)
 
-    SCREEN.fill((255,255,255))
-    hexagon_group.draw(SCREEN)
-    hexagon_group.update()
-    main_classes.camera.update()
+    if GAMESTATE:
+        SCREEN.fill((255,255,255))
+        main_classes.hexagon_group.draw(SCREEN)
+        main_classes.hexagon_group.update()
+        main_classes.camera.update()
--- main_classes.py
+++ main_classes.py
@@ -51,6 +51,8 @@ class Camera(pygame.math.Vector2):
 
 #define game camera object here for hexagon class
 camera = Camera()
+#define the render group for the hexagons
+hexagon_group = pygame.sprite.LayeredUpdates()
 
 class Hexagon(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -77,8 +79,9 @@ class Hexagon(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom = (self.x_pos-camera.x,self.y_pos-camera.y))
         #self.destroy()
 
-    def create_neighboring_polygon(self,side,hexagon,hexagon_group):
-        '''determines the correct xy for the new adjacent polygon'''
+    def position_neighboring_polygon(self,side,hexagon):
+        '''determines the correct xy for the new adjacent polygon.
+        Called by the Render Engine during polygon creation'''
         if side == 1:
             #self.side1 = hexagon
             xylist = [self.x_pos - 28, self.y_pos - 13]
@@ -110,7 +113,8 @@ class Hexagon(pygame.sprite.Sprite):
 
     def correct_position(self,xylist):
         '''called to update the location of a hexagon in relation to it's
-        neighbor after create_neighboring_polygon'''
+        neighbor after position_neighboring_polygon. Called by the render engine
+        during polygon creation'''
         self.x_pos = xylist[0]
         self.y_pos = xylist[1]
         #print(self.x_pos)
@@ -120,21 +124,26 @@ class RenderEngine():
     def __init__(self):
         None
 
-    def create_polygons(self, new_hexagon,realative_hexagon, side,hexagon_group):
-        '''create new polygons and place them correctly. Takes the polygon to be moved,
-        the polygon to give the coords, the side of the og polygon to move to, and the rendering group'''
+    def create_polygons(self, relative_hexagon, side):
+        '''Creates new hexagon and adds to hexagon list before calling the function to
+        positon the polgyon. hexagon_list needs to be inputed into the render engine
+        in sort_hexagons() to render them in the correct order'''
         global hexagon_list
+        new_hexagon = Hexagon(100,100)
         try: hexagon_list.append(new_hexagon)
         except:
             hexagon_list = []
             hexagon_list.append(new_hexagon)
-        if realative_hexagon != None:
-            realative_hexagon.create_neighboring_polygon(side,new_hexagon,hexagon_group)
+        if relative_hexagon != None:
+            relative_hexagon.position_neighboring_polygon(side,new_hexagon)
         else: None
 
-    def sort_hexagons(self, hexagon_list, hexagon_group):
+    def sort_polygons(self, hexagon_list):
         '''takes in a list of hexagons and an empty render group and outputs a
-        full render group from that list based on hexagon y_pos'''
+        full render group from that list based on hexagon y_pos
+        NOTE: Run after each new polygon that is added after game init'''
+        global hexagon_group
+        hexagon_group = pygame.sprite.LayeredUpdates()
         ydictionary = {}
         for i in hexagon_list:
             y = i.y_pos
@@ -145,7 +154,64 @@ class RenderEngine():
             hexagon_list_sorted.append(ydictionary[i])
         for i in hexagon_list_sorted:
             hexagon_group.add(i)
+            print(i.y_pos)
         return hexagon_group
 
-        #if you create a class in a method then it would cease to exist after right?
-        #it would continue to exist if you export it in a list right?
+RenderEngine1 = RenderEngine() #creates an instance of the render engine class for the console
+
+class TextConsole():
+    def __init__(self):
+        None
+
+    def user_command_input(self):
+        command_input = input("Enter Command: ")
+        self.parse_command(command_input)
+
+    def run_command(self, command, arguments):
+        '''runs implemented commands while testing for errors.
+        NOTE: Should break into sub methods/classes'''
+        global hexagon_group, hexagon_list
+        if command == 'create_polygons':
+            try:
+                arguments_list = arguments.split(",")
+            except:
+                print('Error: Enter Integers for Arguments')
+                return None
+            for i in arguments_list:
+                try:
+                    int(i)
+                except:
+                    print("Error: Enter Integers for Arugments")
+                    return None
+            hexagon_number = int(arguments_list[0])
+            side = int(arguments_list[1])
+            if side > 6:
+                print('Error: First argument too big. Hexagons only have 6 sides')
+            RenderEngine1.create_polygons(hexagon_list[hexagon_number-1],side)
+            hexagon_group = RenderEngine1.sort_polygons(hexagon_list)
+            print('Success')
+            #print(hexagon_list)
+            return None
+        if command == 'help':
+            print('unimplemented')
+            #input relative hexagon number and desired side, both in integers
+        else:
+            print('Error: Not a command')
+
+    def parse_command(self, command_input):
+        '''pareses the command_list into command and argument variables while also
+        testing for parenthesis errors'''
+        if command_input.find("(") != -1:
+            command_list = []
+            command_list = command_input.split("(")
+            if command_list[1].find(")") == -1:
+                print('Error: Parenthesis Error')
+                return None
+            else:
+                command_list.append(command_list[1].split(")")[0])
+            command = command_list[0]
+            arguments = command_list[2]
+            self.run_command(command,arguments)
+        else:
+            command = command_input
+            self.run_command(command,None)
